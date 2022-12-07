897974795a8cab3fa4cfb7bab404fc4d96761c3c
M Rifki Muchtar <thea.rm725@gmail.com>
2022-12-07 18:13:08 +0700

    update

--- waveproject.py
+++ waveproject.py
@@ -5,7 +5,8 @@ import numpy as np
 st.sidebar.header("Gelombang")
 wa, wb = st.sidebar.tabs(["Gelombang A", "Gelombang B"])
 st.sidebar.code('''Anggota:
-- Muhammad Rifki Muchtar''')
+- Muhammad Rifki Muchtar
+- Zidni Ajirul Mujib''')
 # Gelombang A
 f_a = wa.slider("Frequency", 0, 10, key="f_a")
 a_a = wa.slider("Amplitudo", 0, 10, key="a_a")
@@ -18,13 +19,13 @@ p_b = (wb.slider("Phase", 0, 180, key="p_b"))*(np.pi/180)
 w_a = 2*np.pi*f_a
 w_b = 2*np.pi*f_b
 
-x =np.arange(0,2,0.01)
+x =np.arange(0,3.01,0.01)
 
-v_a = [round(a_a*np.sin((w_a*t)+p_a), 3) for t in np.arange(0,2,0.01)]
-v_b = [round(a_b*np.sin((w_b*t)+p_b), 3) for t in np.arange(0,2,0.01)]
-resultant = [sum(i) for i in zip(v_a, v_b)]
-df = pd.DataFrame(list(zip(x, v_a, v_b, resultant)), columns =['x', 'v_a', 'v_b', 'resultant'])
+amplitudo_a = [round(a_a*np.sin((w_a*t)+p_a), 3) for t in np.arange(0,np.pi,0.01)]
+amplitudo_b = [round(a_b*np.sin((w_b*t)+p_b), 3) for t in np.arange(0,np.pi,0.01)]
+resultant = [sum(i) for i in zip(amplitudo_a, amplitudo_b)]
+df = pd.DataFrame(list(zip(x, amplitudo_a, amplitudo_b, resultant)), columns =['t', 'amplitudo_a', 'amplitudo_b', 'resultant'])
 col1, col2 = st.columns(2)
-col1.line_chart(data=df, x="x", y="v_a", height=250, use_container_width=True)
-col2.line_chart(data=df, x="x", y="v_b", height=250, use_container_width=True)
-st.line_chart(data=df, x="x", y="resultant")
+col1.line_chart(data=df, x="t", y="amplitudo_a", height=250, use_container_width=True)
+col2.line_chart(data=df, x="t", y="amplitudo_b", height=250, use_container_width=True)
+st.line_chart(data=df, x="t", y="resultant")
